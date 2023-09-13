7d6df0c2588c7c8d3903ad17a6b225c72f99e691
Mathias Nikkel <mathiasnikkel@gmail.com>
2023-09-13 15:33:23 -0300

    feat: setup jwt

--- setup.sh
+++ setup.sh
@@ -6,30 +6,38 @@ kubectl cluster-info --context kind-poc-kong
 
 # Creating Kong Namespace
 kubectl create ns kong
+kubectl create ns poc
+helm repo add kong https://charts.konghq.com
+
 helm install kong kong/kong -f k8s/kong/kong-conf.yml --set proxy.type=NodePort,proxy.http.nodePort=30000,proxy.tls.nodePort=30003 --set ingressController.installCRDs=false --set serviceMonitor.enabled=true --set serviceMonitor.labels.release=promstack --namespace kong
 
-# Creating POC Namespace
-kubectl create ns poc
+kubectl apply -f setup-jwt.yml
+# # Creating POC Namespace
 
-# Creating API Key
-kubectl create secret generic poc-apikey --from-literal=kongCredType=key-auth --from-literal=key=supersecret -n poc # apikey=supersecret
+# # Creating API Key
+# kubectl create secret generic poc-apikey --from-literal=kongCredType=key-auth --from-literal=key=supersecret -n poc # apikey=supersecret
 
-# Creating Service
-kubectl apply -f k8s/service.yml -n poc
+# # Creating Service
+# kubectl apply -f k8s/service.yml -n poc
 
-# Creating Deployment
-kubectl apply -f k8s/deployment.yml -n poc
+# # Creating Deployment
+# kubectl apply -f k8s/deployment.yml -n poc
 
-# Creating Ingress
-kubectl apply -f k8s/ingress.yml -n poc
+# kubectl apply -f k8s/kong/plugins/jwt-auth.yml -n poc
 
-# Kong Stuff
-# Configure Kong Ingress
-# kubectl apply -f k8s/kong/ingress.yml -n poc
+# kubectl apply -f k8s/kong/consumer/jwt-consumer.yml -n poc
+# kubectl apply -f k8s/kong/credentials/jwt-credentials.yml
 
-# Configure Plugins
-kubectl apply -f k8s/kong/plugins/key-auth.yml -n poc
-kubectl apply -f k8s/kong/plugins/rate-limit.yml -n poc
+# # Creating Ingress
+# kubectl apply -f k8s/ingress.yml -n poc
 
-# Configure Consumer
-kubectl apply -f k8s/kong/consumer/poc-consumer.yml -n poc
+# # Kong Stuff
+# # Configure Kong Ingress
+# # kubectl apply -f k8s/kong/ingress.yml -n poc
+
+# # Configure Plugins
+# kubectl apply -f k8s/kong/plugins/key-auth.yml -n poc
+# kubectl apply -f k8s/kong/plugins/rate-limit.yml -n poc
+
+# # Configure Consumer
+# kubectl apply -f k8s/kong/consumer/poc-consumer.yml -n poc
